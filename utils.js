c6f7c5b7f5f8871a49e5d8d16f7124a36546b377
Gloria Saija <blackiedoescomics@gmail.com>
2024-11-02 11:59:37 +0100

    chore: separation of concerns

---
+++ utils.js
@@ -0,0 +1,38 @@
+export const clearActive = () => {
+  document
+    .querySelectorAll("li.active")
+    .forEach((li) => li.classList.remove("active"));
+};
+
+export const scrollToSection = (sectionClass, activeId) => {
+  document
+    .querySelector(`.${sectionClass}`)
+    .scrollIntoView({ behavior: "smooth" });
+  clearActive();
+  document.querySelector(`#${activeId}`).classList.add("active");
+};
+
+export const toggleMenu = () => {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("translate-y-[0]");
+  nav.classList.toggle("bg-white");
+  document.querySelectorAll(".line").forEach((line, index) => {
+    line.classList.toggle(
+      `rotate-[${index === 0 ? "45deg" : index === 1 ? "0" : "-45deg"}]`
+    );
+  });
+};
+
+export const handleScroll = () => {
+  const nav = document.querySelector("nav");
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      nav.style.transform = "translateY(-50%)";
+    } else {
+      nav.style.transform = "translateY(0%)";
+    }
+  };
+};
